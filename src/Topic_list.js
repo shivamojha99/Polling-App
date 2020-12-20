d4de523471ad562361a18758577a4c1da90ee8d1
lucky4030 <lucky@.github.com>
2020-12-21 03:11:37 +0530

    added thumb up and down feature with rating bar

--- src/Topic_list.js
+++ src/Topic_list.js
@@ -1,12 +1,16 @@
 import React,{useState} from 'react';
 import db from './Firebase';
 import "./topic_list.css";
-import AddIcon from '@material-ui/icons/Add';
-import Fab from '@material-ui/core/Fab';
-import RemoveIcon from '@material-ui/icons/Remove';
-import { CardActions, CardContent, Grid } from "@material-ui/core"
+// import AddIcon from '@material-ui/icons/Add';
+// import Fab from '@material-ui/core/Fab';
+// import RemoveIcon from '@material-ui/icons/Remove';
+import { CardActions, CardActionArea, CardContent, Grid } from "@material-ui/core"
 import Card from '@material-ui/core/Card';
 import { useStateValue } from "./StateProvider";
+
+import LinearProgress from '@material-ui/core/LinearProgress';
+import ThumbUpOutlinedIcon from '@material-ui/icons/ThumbUpOutlined';
+import ThumbDownAltOutlinedIcon from '@material-ui/icons/ThumbDownAltOutlined';
 
 function Topic_list(props) {
 const [{canVote}, dispatch] = useStateValue(); 
@@ -45,20 +49,38 @@ const Judge2 =()=>{
         <>
         <CardContent>
             <div className="middle">
-                <p>{props.topicc}</p>  
+                <p>{props.topicc}</p> 
+            </div> 
+        </CardContent>
+        <CardActions>   
+            <div className="positiveBar" >
+                <div id="thumbUp" >
+                    <ThumbUpOutlinedIcon onClick={Judge1} />
+                    {props.pos}
+                </div>
+                <LinearProgress id="posBar" variant="determinate" color="primary" value={props.pos} />
             </div>
-                <CardActions>
+            <div className="negativeBar" >
+                <div id="thumbdown">
+                    <ThumbDownAltOutlinedIcon onClick={Judge2} />
+                    {- props.neg}
+                </div>
+                <LinearProgress id="negBar" variant="determinate" color="secondary" value={- props.neg }  />
+            </div>
+        </CardActions>
+                {/* <CardActions>
                 <div className="left"> 
                 <h4>{props.pos}</h4>
                 <Fab size="small" color="primary" aria-label="add">
                 <AddIcon onClick={Judge1}></AddIcon></Fab>
                 </div>
                 
-            <div className="right">
-            <h4>{props.neg}</h4>
-            <Fab size="small" color="secondary" aria-label="remove">
-            <RemoveIcon   onClick={Judge2}></RemoveIcon></Fab></div></CardActions>         
-        </CardContent> 
+                <div className="right">
+                <h4>{props.neg}</h4>
+                <Fab size="small" color="secondary" aria-label="remove">
+                <RemoveIcon   onClick={Judge2}></RemoveIcon></Fab></div>
+                </CardActions>          */}
+         
         </>
         </Card>
         </div>
